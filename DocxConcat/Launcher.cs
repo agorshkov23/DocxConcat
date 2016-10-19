5fab6269b6c4e4bdc5ad914f1e0900e8aacd17a9
Alexey Gorshkov <agorshkov23@yandex.ru>
2016-10-19 08:53:10 +0300

    Implementation of generating and reading the DocxConcat project file

--- DocxConcat/Launcher.cs
+++ DocxConcat/Launcher.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
 
 namespace Alegor.DocxConcat
 {
     internal class Launcher
     {
-        public static void Main(string[] args)
+        private const string GenerateProjectFile = "-GenerateProjectFile";
+
+        public static int Main(string[] args)
         {
-            throw new NotImplementedException();
+            if (args.Length == 1)
+            {
+                var serializerSettings = new DataContractJsonSerializerSettings()
+                {
+                    UseSimpleDictionaryFormat = true
+                };
+
+                var serializer = new DataContractJsonSerializer(typeof(Properties), serializerSettings);
+
+                Properties properties = null;
+                using (var fileStream = new FileStream(args[0], FileMode.Open))
+                {
+                    properties = serializer.ReadObject(fileStream) as Properties;
+                }
+
+                if (properties == null)
+                {
+                    Console.Error.WriteLine("Error reading DocxConcat project file!");
+                }
+
+                var program = new Program(properties);
+                program.Run();
+
+                return 0;
+            }
+
+            if (args.Length == 2 && GenerateProjectFile.Equals(args[0], StringComparison.CurrentCultureIgnoreCase))
+            {
+                var serializerSettings = new DataContractJsonSerializerSettings()
+                {
+                    UseSimpleDictionaryFormat = true
+                };
+
+                var properties = new Properties();
+                var serializer = new DataContractJsonSerializer(typeof(Properties), serializerSettings);
+
+                using (var fileStream = new FileStream(args[1], FileMode.OpenOrCreate))
+                {
+                    serializer.WriteObject(fileStream, properties);
+                }
+
+                return 0;
+            }
+
+            Console.WriteLine("DocxConcat");
+            Console.WriteLine();
+            Console.WriteLine("Using:");
+            Console.WriteLine("    DocxConcat.exe ProjectFile");
+            Console.WriteLine("    DocxConcat.exe -GenerateProjectFile");
+
+            return 0;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DocxConcat
+
 Program to concatenate files of the docx format (Microsoft Office Word Document).
+
+## Using
+
+    DocxConcat.exe ProjectFile
+
+    DocxConcat.exe -GenerateProjectFile
